e69175b32cdcab2100e176b946ca2ed18ae3e06d
shaikhmubin02 <shaikhmubin572@gmail.com>
2024-08-09 06:35:30 +0530

    init

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,27 +1,43 @@
-"use client"
+"use client";
 
-import { signIn, signOut, useSession } from 'next-auth/react'
-import React from 'react'
-import {PrimaryButton, SecondaryButton} from './Button';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import React, { useState } from 'react';
+import { PrimaryButton } from './Button';
+import { useRouter } from 'next/navigation';
 
 const Appbar = () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [isSigningOut, setIsSigningOut] = useState(false);
 
-  const session = useSession();
+  const handleSignOut = async () => {
+    setIsSigningOut(true); // Immediately change the button to "Sign In"
+    try {
+      await signOut({ redirect: false }); // Prevent NextAuth's default redirect
+      router.push('/'); // Manually redirect after updating the UI
+    } catch (error) {
+      console.error("Sign out error:", error);
+    }
+  };
+
   return (
     <div className='border-b px-2 py-2 flex justify-between'>
-        <div className='text-xl font-bold flex flex-col justif-center'>
-            NexTrade
-        </div>
-        <div>
-            {session.data?.user ? <PrimaryButton onClick={()=>  
-              signOut()
-            }>Logout</PrimaryButton> : 
-            <PrimaryButton onClick={()=>  
-              signIn('google')
-            }>Signin</PrimaryButton> }
-        </div>
+      <div className='text-xl font-bold flex flex-col justify-center'>
+        NexTrade
+      </div>
+      <div>
+        {session?.user && !isSigningOut ? (
+          <PrimaryButton onClick={handleSignOut}>
+            Logout
+          </PrimaryButton>
+        ) : (
+          <PrimaryButton onClick={() => signIn('google')}>
+            Sign In
+          </PrimaryButton>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Appbar
+export default Appbar;
